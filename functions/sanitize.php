0c49e19b9bd9a1b4651aa9c9c7c51ba3c7277fae
Janis Gura <55707203+jasss777@users.noreply.github.com>
2019-12-03 09:04:08 +0200

    Update sanitize.php

--- functions/sanitize.php
+++ functions/sanitize.php
@@ -1,4 +1,7 @@
 <?php
+ /**
+ * escape funcion - convert double quotes and use character set - West Europe.
+ */ 
 function escape($string){
     return htmlentities($string, ENT_QUOTES, 'UTF-8');
 }
