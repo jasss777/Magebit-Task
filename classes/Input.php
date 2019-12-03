c1ebd10d0f738a812a720aaad1e9b0cb2f5fac45
Janis Gura <55707203+jasss777@users.noreply.github.com>
2019-12-03 09:02:10 +0200

    Update Input.php

--- classes/Input.php
+++ classes/Input.php
@@ -1,5 +1,12 @@
 <?php
 class Input {
+
+    /**
+    *  method exist have only two types, POST = send data to db, GET = get data from db
+    * 1) check if type post field is  not empty, true false
+    * 2) check if type get field is not empty, true false
+    * 3) in other types return false
+    */ 
     public static function exists($type = 'post'){
         switch($type){
             case 'post':
@@ -13,6 +20,13 @@ class Input {
             break;
         }
     }
+
+    /**
+    *  method get - require $item (value in db)
+    * 1) check if set data is POST than send $item to db
+    * 2) check if set data is GET than return $item from db
+    * 3) else return '';
+    */ 
     public static function get($item){
         if 
         (isset($_POST[$item])) 
