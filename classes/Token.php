e1ab318f1147177b0143b9a66e44bafdb526ec49
Janis Gura <55707203+jasss777@users.noreply.github.com>
2019-12-03 09:02:50 +0200

    Update Token.php

--- classes/Token.php
+++ classes/Token.php
@@ -1,9 +1,16 @@
 <?php
 class Token{
+     /**
+      * method generate - generate random token
+     */ 
     public static function generate(){
         return Session::put(Config::get('session/token_name'), md5(uniqid()));
     }
 
+     /**
+    * method check - checking or token exists and in session have the same token
+    * if token exists delete them and return true.
+    */ 
     public static function check($token){
         $tokenName = Config::get('session/token_name');
 
