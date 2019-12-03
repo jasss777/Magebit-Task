89960fa7461879c87aa95a978b1357a2c80b3b46
Janis Gura <55707203+jasss777@users.noreply.github.com>
2019-12-03 09:01:54 +0200

    Update Hash.php

--- classes/Hash.php
+++ classes/Hash.php
@@ -1,10 +1,16 @@
 <?php
 class Hash {
 
+    /**
+    * method make - randomly generate data
+    */ 
     public static function make($string, $salt = ''){
         return hash('sha256', $string . $salt);
     }
 
+    /**
+    * must return mcrypt_create_iv, but not working.
+    */ 
     public static function salt($length){
         return rand($length);
     }
